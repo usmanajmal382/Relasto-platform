0f3e9055360b8ff2fe4c0926479ac4c379b86e78
Awais <awais@example.com>
2026-05-06 16:11:58 +0500

    Simplify Vercel config and remove build script

--- backend/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Building the project..."
-python3 -m pip install -r requirements.txt
-
-echo "Running Migrations..."
-python3 manage.py migrate --noinput
-
-echo "Collecting Static Files..."
-python3 manage.py collectstatic --noinput --clear
