f114aedc3494b3816d27456cebae1a88aebfc679
Awais <awais@example.com>
2026-05-06 16:03:36 +0500

    Update build script to use python3

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
+
 echo "Running Migrations..."
-python3.12 manage.py migrate --noinput
+python3 manage.py migrate --noinput
+
 echo "Collecting Static Files..."
-python3.12 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
