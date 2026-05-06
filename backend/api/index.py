93a02676773032dd752c5dae75850509dcdbbbed
Awais <awais@example.com>
2026-05-06 16:24:58 +0500

    Fix sys.path in api/index.py

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from backend_core.wsgi import app
 
 # This is the entry point for Vercel
